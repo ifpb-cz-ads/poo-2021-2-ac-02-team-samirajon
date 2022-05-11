3941e3261a1c494a5bfbc5651e3176017c29908a
Samira <samieditor2020@gmail.com>
2022-05-11 13:32:53 -0300

    CBB &  BM answers

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,12 @@
+
+class CBBAtividadePratica4 {
+  public static void main(String[] args) {
+    
+    float cubagemPiscina = 5;
+    float precoMetroCubico = 100;
+    float valorTotal;
+
+    valorTotal = precoMetroCubico * cubagemPiscina;
+    System.out.println("R$ " + valorTotal);
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-
+1. a) F
+b) V
+c) F
+d) V
+e) F
+f) F
+g) V
+h) V
+i) F
+j) F
